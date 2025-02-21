c63cf3c376a1ef38c873928ad9f64713b8545448
turbo2121 <eyobaddis394@gmail.com>
2025-02-21 12:46:03 +0300

    Fixed another cors  issues

--- menu-management-backend/src/main.ts
+++ menu-management-backend/src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Enable CORS for all origins
   app.enableCors({
-    origin: 'http://localhost:3001, https://menu12.vercel.app', // Allow requests from your Next.js frontend
+    origin: ['http://localhost:3001', 'https://menu12.vercel.app'], // Allow requests from your Next.js frontend
     credentials: true, // If you're using cookies or authentication
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allow specific methods
     allowedHeaders: 'Content-Type, Authorization', // Allow specific headers
